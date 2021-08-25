e534f45d17850aa00fda85a6bd8032a750736d95
Kanu <begintofly@protonmail.com>
2021-08-25 20:01:06 +0900

    계정 테이블 추가

--- src/main/resources/db/mysql/data.sql
+++ src/main/resources/db/mysql/data.sql
@@ -16,3 +16,36 @@ SET regDate    = NOW(),
     updateDate = NOW(),
     title      = '제목 3',
     content     = '내용 3';
+
+-- # 계정, 테스트 데이터 생성 (관리자 계정)
+INSERT INTO `account`
+SET regDate      = NOW(),
+    updateDate   = NOW(),
+    username     = 'admin',
+    `password`   = 'admin',
+    authLevel    = 7,
+    `name`       = '관리자',
+    nickname     = '관리자',
+    mobileNumber = '01011112222',
+    email        = 'admin@email.com';
+
+-- # 계정, 테스트 데이터 생성 (일반회원 계정)
+INSERT INTO `account`
+SET regDate      = NOW(),
+    updateDate   = NOW(),
+    username     = 'user1',
+    `password`   = 'user1',
+    `name`       = '사용자1',
+    nickname     = '사용자1',
+    mobileNumber = '01011112222',
+    email        = 'user1@email.com';
+
+INSERT INTO `account`
+SET regDate      = NOW(),
+    updateDate   = NOW(),
+    username     = 'user2',
+    `password`   = 'user2',
+    `name`       = '사용자2',
+    nickname     = '사용자2',
+    mobileNumber = '01011112222',
+    email        = 'user2@email.com';
