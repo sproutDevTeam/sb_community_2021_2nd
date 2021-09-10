6ece37ab6c2e55e41bc084a5830af36ed4cac203
Kanu <begintofly@protonmail.com>
2021-09-10 22:54:42 +0900

    CommonInterceptor 요청, 응답 관련 로그출력 기능
    - 클라이언트 요청 정보
      - 요청 URI
      - 요청 파라미터
      - 클라이언트 IP
      - 해당 요청과 맵핑된 컨트롤러 및 핸들러 메서드명
      - 맵핑된 VIEW 이름
    
    - 요청 로그 출력 시, 패스워드와 같은 민감한 파라미터 값은 * 로 숨김

--- src/main/java/com/tena/sbcommunity2021/global/interceptors/CommonInterceptor.java
+++ src/main/java/com/tena/sbcommunity2021/global/interceptors/CommonInterceptor.java
@@ -2,11 +2,13 @@ package com.tena.sbcommunity2021.global.interceptors;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
+import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Enumeration;
 
 @Slf4j
 @Component
@@ -15,28 +17,76 @@ public class CommonInterceptor implements HandlerInterceptor {
 	// 요청(Request)과 맵핑된 Handler 실행 전 처리사항
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-		log.info("CommonInterceptor.preHandle");
-		log.info("{} 를 호출했습니다.", handler.toString());
+		log.debug("requestURI: {}", request.getRequestURI());
+		log.debug("contextPath: {}", request.getContextPath());
 
-		return HandlerInterceptor.super.preHandle(request, response, handler);
+		if (handler instanceof HandlerMethod) {
+			HandlerMethod handlerMethod = (HandlerMethod) handler;
+			log.info("[preHandle][{}][{}{}][{}][{}]", request.getMethod(), request.getRequestURI(), getParameters(request), handlerMethod.getBeanType().getSimpleName(), handlerMethod.getMethod().getName());
+		}
+
+		return true;
 	}
 
 	// View 렌더링 전 처리사항
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-		String viewName = (modelAndView != null) ? modelAndView.getViewName() : null;
-		log.info("CommonInterceptor.postHandle");
-		log.info("{} 가 종료됐습니다. {} 를 view 로 사용합니다.", handler.toString(), viewName);
-
-		HandlerInterceptor.super.postHandle(request, response, handler, modelAndView);
+		log.info("[postHandle][{}][{}{}]", request.getMethod(), request.getRequestURI(), getParameters(request));
 	}
 
 	// View 렌더링 후 처리사항, 클라이언트에 최종적인 응답(Response) 전달
 	@Override
 	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception {
-		log.info("CommonInterceptor.afterCompletion");
+		if (ex != null) { // ※ 참고: 만약 @ExceptionHandler 를 통해 ex 를 처리하고 있다면 null 이 잡힘. 즉 예외가 여기까지 넘어오지 않음
+			ex.printStackTrace();
+			log.info("[afterCompletion][{}][{}{}][exception: {}]", request.getMethod(), request.getRequestURI(), getParameters(request), ex.getClass().getSimpleName());
+		}
+	}
 
-		HandlerInterceptor.super.afterCompletion(request, response, handler, ex);
+	// 파라미터를 쿼리스트링 형태로 리턴
+	private String getParameters(HttpServletRequest request) {
+		StringBuffer posted = new StringBuffer();
+		Enumeration<?> parameterNames = request.getParameterNames();
+
+		if (parameterNames != null) {
+			posted.append("?");
+		}
+
+		while (parameterNames.hasMoreElements()) {
+			if (posted.length() > 1) {
+				posted.append("&");
+			}
+
+			String parameterName = (String) parameterNames.nextElement();
+
+			posted.append(parameterName + "=");
+
+			// 패스워드와 같은 민감한 값들 별표로
+			if (parameterName.contains("password") || parameterName.contains("pass") || parameterName.contains("pwd")) {
+				posted.append("*****");
+			} else {
+				posted.append(request.getParameter(parameterName));
+			}
+		}
+
+		String ipAddr = getRemoteAddr(request);
+
+		if (ipAddr != null && !ipAddr.equals("")) {
+			posted.append("&_psip=" + ipAddr);
+		}
+		return posted.toString();
+	}
+
+	// 클라이언트 IP 주소 리턴
+	private String getRemoteAddr(HttpServletRequest request) {
+		String ipFromHeader = request.getHeader("X-FORWARDED-FOR");
+
+		if (ipFromHeader != null && ipFromHeader.length() > 0) {
+			log.debug("ip from proxy - X-FORWARDED-FOR : " + ipFromHeader);
+			return ipFromHeader;
+		}
+
+		return request.getRemoteAddr();
 	}
 
 }
